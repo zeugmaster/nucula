e8b3a55ec2b277744513bf5896be87523ead675b
Dario <zeugmaster@protonmail.com>
2026-02-23 08:42:00 +0100

    added secp

--- main/nucula.c
+++ main/nucula.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <esp_log.h>
+#include "secp256k1.h"
+#include "secp256k1_schnorrsig.h"
+#include "secp256k1_extrakeys.h"
 
 void app_main(void)
 {
     ESP_LOGI("nucula", "running main");
+
+
+    secp256k1_context *ctx = secp256k1_context_create(SECP256K1_CONTEXT_NONE);
+    ESP_LOGI("nucula", "secp256k1 context: %p", (void *)ctx);
+    secp256k1_context_destroy(ctx);
 }
